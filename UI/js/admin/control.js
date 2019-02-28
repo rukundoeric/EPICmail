b7c48f105550d8ab98c51c3ada4595ef92593812
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-02-28 11:37:41 +0200

    [Feature #164297407] enable modal control

---
+++ UI/js/admin/control.js
@@ -0,0 +1,14 @@
+// Get the modal
+var modal = document.getElementById('add_group_dialog');
+// Get the button that opens the modal
+var btn = document.getElementById("add_new_group_btn");
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}

6d431d6864f12a0d869799df55a00e9e7eb4c3b7
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-08 19:56:04 +0200

    [Feature #164502656] Api enable to send message

--- server/router/Message.js
+++ server/router/Message.js
@@ -3,5 +3,6 @@ import auth from '../middleware/auth';
 import message from '../controllers/Message'
 import {apiUrlv1messages, apiUrlv1messagesUnread,apiUrlv1messagesSent, apiUrlv1messagesAction} from '../helpers/const'
 const router = express.Router();
+router.post(`${apiUrlv1messages}`,auth.verifyToken,message.createMessage);
 router.get(`${apiUrlv1messages}`,auth.verifyToken,message.getAllReceivedMessages);
 export default router;

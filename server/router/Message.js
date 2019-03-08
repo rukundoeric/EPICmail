1ccbab66260803df8ed8a8465dd3fec95d9ae194
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-08 19:39:23 +0200

    [Feature #164502024] API enable user to fetch all received message

---
+++ server/router/Message.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import auth from '../middleware/auth';
+import message from '../controllers/Message'
+import {apiUrlv1messages, apiUrlv1messagesUnread,apiUrlv1messagesSent, apiUrlv1messagesAction} from '../helpers/const'
+const router = express.Router();
+router.get(`${apiUrlv1messages}`,auth.verifyToken,message.getAllReceivedMessages);
+export default router;

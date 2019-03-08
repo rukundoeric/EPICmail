509da33e4b8103111d8804072b1806be048969a2
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-08 18:28:03 +0200

    [Chore #164498719] setup swagger docs

--- server/config/server.js
+++ server/config/server.js
@@ -4,13 +4,18 @@ import User from '../router/User';
 import Message from '../router/Message';
 import bodyParcer from 'body-parser';
 import dotenv from 'dotenv';
+import swagger from 'swagger-ui-express';
+import yamljs from 'yamljs';
 dotenv.config();
+process.env.IS_TESTING = 'FALSE';
+const swaggerDocument = yamljs.load('server/config/docs.yml');
 const app = express();
 const PORT = process.env.PORT || 7070;
 app.use(bodyParcer.json());
 app.use(bodyParcer.urlencoded({ extended: false }));
 app.use('/',User);
 app.use('/',Message);
+app.use('/', swagger.serve, swagger.setup(swaggerDocument));
 app.use((err, req, res, next) => {
     res.status(err.status || ST.INTERNAL_S_E);
     res.json({

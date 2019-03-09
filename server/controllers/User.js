7e896dc73694a7de6af16d6fd27f5e630ead731e
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-09 13:36:58 +0200

    [Feature #164194713] login user controller:

---
+++ server/controllers/User.js
@@ -0,0 +1,53 @@
+import moment from 'moment';
+import ST from '../helpers/status'
+import MSG from '../helpers/res_messages'
+import helper from '../helpers/Helper'
+import userModel from '../models/User';
+import auth from '../middleware/auth';
+import Helper from '../helpers/Helper';
+import dotenv from 'dotenv';
+class User {
+    constructor(){
+        dotenv.config();
+    }
+    async login(req, res, next){
+         if (!req.body.email || ! req.body.password){
+             return res.status(ST.BAD_REQUEST).send(MSG.MSG_WRONG_INPUTS);
+         }
+         else{
+
+            if(!Helper.isValidEmail(req.body.email)){
+               return res.status(ST.BAD_REQUEST).send(MSG.MSG_WRONG_INPUTS);
+            }
+            else{
+                   
+                   
+                    userModel.getUserByEmail(req.body.email).then((user) => {
+                        if(!user){
+                            return res.status(ST.NOT_FOUND).send(MSG.MSG_NO_USER_EXIST);
+                        }else{
+                            Helper.isCorrestPassword(req.body.password,user.password).then((result) => {
+  
+                                if(!result){
+                                    return res.status(ST.BAD_REQUEST).send(MSG.MSG_WRONG_PASSWORD);
+                                }else{
+                                    
+                                    auth.generateToken(user).then((token) => {
+                                
+                                        return res.status(ST.OK).send({
+                                            "status" : ST.OK,
+                                            "data" : [{"token" : token}]
+                                        });
+                        
+                                    });
+                                }
+                            });
+                        }
+                    })
+                   
+            }
+         }   
+         
+    }
+}
+export default new User();

672d34974cbd7dfa99e46631b636a1b0300cc91a
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-09 13:06:15 +0200

    Conflict resolved

--- router/User.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import user from '../controllers/User';
-import auth from '../middleware/auth';
-import {apiUrlv1authLogin, apiUrlv1authSignup} from '../helpers/const'
-const router = express.Router();
-router.post(`${apiUrlv1authSignup}`, user.createUser);
-export default router;

d8d78856ace506b3385246abc5588d6b945216a0
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-17 04:02:29 +0200

    check connection

--- server/router/User.js
+++ server/router/User.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import user from '../controllers/User';
+import dbUser from '../db/controllers/User';
 import {apiUrlv1authLogin, apiUrlv1authSignup ,apiUrlv2authSignup, apiUrlv2authLogin} from '../helpers/const'
 const router = express.Router();
 router.get('/api/v1/users', user.getAllUser);
 //API version1
 router.post(`${apiUrlv1authSignup}`, user.createUser);
 router.post(`${apiUrlv1authLogin}`, user.login);
-
+router.get('/testCon', dbUser.checkcon);
 //API version2
 // router.post(`${apiUrlv2authSignup}`, userdb.createUser);
 // router.post(`${apiUrlv2authLogin}`, userdb.login);

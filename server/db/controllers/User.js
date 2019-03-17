232ee08cd41c3eda63bcd881a68268424abd41f8
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-17 04:05:32 +0200

    check connection

--- server/db/controllers/User.js
+++ server/db/controllers/User.js
@@ -1,13 +1,6 @@
 import jwt from 'jsonwebtoken';
 import moment from 'moment';
 import uuidv4 from 'uuid/v4';
-import ST from '../../helpers/status'
-import MSG from '../../helpers/res_messages'
-import auth from '../middleware/auth';
-import Helper from '../helpers/Helper';
-import dotenv from 'dotenv';
-import joi from 'joi';
-import validation from '../../helpers/validation';
 import db_connection from '../connection/connection'
 class User {
      constructor(){

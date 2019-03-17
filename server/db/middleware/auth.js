68664b127763cee82c2b67b3fc4e76e9204989b5
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-17 21:35:14 +0200

    [Feature #164681066] Enable Authentication

---
+++ server/db/middleware/auth.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { GET_USER_BY_ID } from '../helpers/query';
+import ST from '../helpers/status';
+import MSG from '../helpers/res_messages';
+class Auth {
+    constructor(){
+        dotenv.config();
+    }
+    async verifyToken(req, res, next){
+        let token =  req.headers['epic-mail-access-token'];
+        if(!token){
+            return res.status(ST.BAD_REQUEST).send({
+                "status" : ST.BAD_REQUEST,
+                "error" : "Token is not provided"
+            });
+        }
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+        const { rows } = await db.query(GET_USER_BY_ID, [decoded.userid]);
+        if(!rows[0]) {
+          return res.status(ST.BAD_REQUEST).send({ 
+              "status":ST.BAD_REQUEST,
+              "error": {'message': 'The token you provided is invalid'}
+           });
+        }
+        req.user = { id: decoded.userId };
+        next();
+    }
+    async generateToken(user) {
+       const token = jwt.sign({userid: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+       return token;
+    }
+    async getIdfromToken(token){
+      let UserId = await jwt.verify(token, process.env.JWT_SECRET).userid
+      return UserId;
+    }
+}
+export default new Auth();

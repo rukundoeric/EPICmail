12fa8ceaf82f0dc625b4b269ff4f90aa71ff767c
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-17 20:31:15 +0200

    [Feature #164194249] Query executer

--- server/db/db.js
+++ server/db/db.js
@@ -1,16 +1,20 @@
 import dotenv from 'dotenv';
-import { pool } from './connection/connection';
+import connection from './connection/connection'
+const pool = connection.getPoolConnection();
 dotenv.config();
-export default {
-  query(text, params){
-    return new Promise((resolve, reject) => {
-      pool.query(text, params)
-      .then((res) => {
-        resolve(res);
+class ExecuteQuery{
+  constructor(){
+    this.query = (text, params) => {
+      return new Promise((resolve, reject) => {
+        pool.query(text, params)
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          reject(err);
+        })
       })
-      .catch((err) => {
-        reject(err);
-      })
-    })
+    }
   }
 }
+export default new ExecuteQuery();

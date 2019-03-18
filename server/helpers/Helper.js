002056d123b91f6dd7fbd04eb7153b33242ebb38
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-18 20:20:34 +0200

    removed duplication

--- server/helpers/Helper.js
+++ server/helpers/Helper.js
@@ -7,12 +7,6 @@ class Helper {
         this.isValidEmail = email => {
             return /\S+@\S+\.\S+/.test(email);
         }
-        this.isStrongPassword = password => {
-            // at least one number, one lowercase and one uppercase letter
-            // at least six characters that are letters, numbers or the underscore
-            var st_pass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{8,}$/;
-            return st_pass.test(password);
-        }
     }
     async hashPassword(password){
         return  bcrpt.hash(password, bcrpt.genSaltSync(6));

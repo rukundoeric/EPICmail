049714089d2f5628a9a31c72257c8d68e5742b08
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-19 23:44:57 +0200

    [Feature #164680453] Delete group

--- server/db/controllers/Group.js
+++ server/db/controllers/Group.js
@@ -5,7 +5,11 @@ import moment from 'moment';
 import joi from 'joi';
 import db from '../db'
 import validation from '../../helpers/validation';
-import { CREATE_GROUP_RECORD, CREATE_GROUP_MEMBER_RECORD } from '../helpers/query'
+import { CREATE_GROUP_RECORD, 
+  CREATE_GROUP_MEMBER_RECORD,
+  GET_GROUP_MEMBER,
+  DELETE_GROUP,
+  DELETE_GROUP_MEMBER} from '../helpers/query'
 class Group{
    async createGroup(req, res){
     joi.validate(req.body, validation.Validator.groupSchema).then(() => {
@@ -21,6 +25,30 @@ class Group{
       "status": 400,
       "error": {"message": error.details[0].message.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '')}
     }));    
+   }
+   async deletegroup(req, res){
+    let id= req.params;
+    joi.validate(id, validation.Validator.getOrDelMsgSchema).then((result) => {
+      db.query(GET_GROUP_MEMBER,[id.id, req.user.id]).then((member) => {
+        if(member.rows[0].role != 'owner'){
+          res.status(ST.UNAUTHORIZED).send({
+            "status": ST.UNAUTHORIZED,
+            "error": MSG.MSG_PRGS_DELETE_GROUP
+          }); 
+        }else{
+          db.query(DELETE_GROUP, [id.id]).then(() => {
+            db.query(DELETE_GROUP_MEMBER, [id.id, req.user.id]);
+            res.status(ST.OK).send({
+              "status": ST.OK,
+              "data": MSG.MSG_GROUP_DELETED_SUCCESSFUL
+            });
+          })
+        }
+      })
+    }).catch(error => res.send({
+      "status": 400,
+      "error": {"message": error.details[0].message.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '')}
+    })); 
    }
 }
 export default new Group();

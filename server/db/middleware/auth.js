a6d9cde654fb6feff1db9465da0e54212639622b
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-19 15:45:37 +0200

    [Feature #164502656] Updated authentication

--- server/db/middleware/auth.js
+++ server/db/middleware/auth.js
@@ -4,11 +4,13 @@ import { GET_USER_BY_ID } from '../helpers/query';
 import ST from '../../helpers/status';
 import MSG from '../../helpers/res_messages';
 import db from '../db'
+import { decode } from 'punycode';
 class Auth {
     constructor(){
         dotenv.config();
     }
     async verifyToken(req, res, next){
+      try{
         let token =  req.headers['epic-mail-access-token'];
         if(!token){
             return res.status(ST.BAD_REQUEST).send({
@@ -18,16 +20,21 @@ class Auth {
         }
         const decoded = await jwt.verify(token, process.env.JWT_SECRET);
         db.query(GET_USER_BY_ID, [decoded.userid]).then((result) => {
-            console.log(result)
-            // if(!result.rows[0]) {
-            //     return res.status(ST.BAD_REQUEST).send({ 
-            //         "status":ST.BAD_REQUEST,
-            //         "error": {'message': 'The token you provided is invalid'}
-            //      });
-            //   }
-            //   req.user = { id: decoded.userId };
-            //   next();
+            if(!result.rows[0]) {
+                return res.status(ST.BAD_REQUEST).send({ 
+                    "status":ST.BAD_REQUEST,
+                    "error": {'message': 'The token you provided is invalid'}
+                 });
+              }
+              req.user = { id:result.rows[0].id };
+              next();
         })
+      }catch(error){
+        return res.status(ST.BAD_REQUEST).send({ 
+            "status":ST.BAD_REQUEST,
+            "error": {'message': 'The token you provided is invalid'}
+         });
+      }
     }
     async generateToken(user) {
        const token = jwt.sign({userid: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });

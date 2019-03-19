dc42b0524bea8bbe084e89eb2571a8958d23c7b4
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-19 22:22:31 +0200

    [Feature #164680431] Added validation

--- server/helpers/validation.js
+++ server/helpers/validation.js
@@ -1,31 +1,39 @@
 import joi from 'joi';
 class Validation {
-    constructor(){
-        this.Validator = {
-            loginSchema: joi.object().keys({
-                email: joi.string().email().required(),
-                password:  joi.string().regex(/^[a-zA-Z]/).min(8).required(),
-            }),
-            userSchema: joi.object().keys({
-                email: joi.string().email().required(),
-                firstName: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
-                lastName:  joi.string().regex(/^[a-zA-Z]/).min(1).required(),
-                password:  joi.string().regex(/^[a-zA-Z]/).min(8).required(),
-            }),
-            messageSchema: joi.object().keys({
-                subject: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
-                message: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
-                to: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
-                status: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
-            }),
-            getOrDelMsgSchema: joi.object().keys({
-                id: joi.string().regex(/^[0-9]+$/).min(1).required()
-            }),
-            verificationSchema: joi.object().keys({
-                email: joi.string().email().required(),
-                code:  joi.string().regex(/^[0-9]+$/).min(5).required(),
-            }),
-        } 
-    }
+  constructor(){
+    this.Validator = {
+      loginSchema: joi.object().keys({
+          email: joi.string().email().required(),
+          password:  joi.string().regex(/^[a-zA-Z]/).min(8).required(),
+      }),
+      userSchema: joi.object().keys({
+          email: joi.string().email().required(),
+          firstName: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
+          lastName:  joi.string().regex(/^[a-zA-Z]/).min(1).required(),
+          password:  joi.string().regex(/^[a-zA-Z]/).min(8).required(),
+      }),
+      messageSchema: joi.object().keys({
+          subject: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
+          message: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
+          to: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
+          status: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
+      }),
+      getOrDelMsgSchema: joi.object().keys({
+          id: joi.string().regex(/^[0-9]+$/).min(1).required()
+      }),
+      verificationSchema: joi.object().keys({
+          email: joi.string().email().required(),
+          code:  joi.string().regex(/^[0-9]+$/).min(5).required(),
+      }),
+      groupSchema: joi.object().keys({
+          name: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
+      }),
+      groupMemberSchema: joi.object().keys({
+          groupid: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
+          memberid: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
+          role: joi.string().regex(/^[a-zA-Z]/).min(1).required(),
+      }),
+    } 
+  }
 }
 export default new Validation;

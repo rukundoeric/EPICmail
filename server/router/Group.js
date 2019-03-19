256c471676bd1a1f2551c27b4670d8dea2d55cbf
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-19 23:47:25 +0200

    [Feature #164680453] api enable delete group

--- server/router/Group.js
+++ server/router/Group.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import authv2 from '../db/middleware/auth'
 import group from '../db/controllers/Group'
-import {apiUrlv2createGroup} from '../helpers/const'
+import {apiUrlv2createGroup,
+  apiUrlv2deleteGroup} from '../helpers/const'
 const router = express.Router();
 router.post(`${apiUrlv2createGroup}`,authv2.verifyToken,group.createGroup);
+router.delete(`${apiUrlv2deleteGroup}`,authv2.verifyToken,group.deletegroup);
 export default router;

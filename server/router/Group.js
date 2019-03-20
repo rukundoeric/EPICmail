a2f387881687a00f7f07630de125e2cadf1b02f2
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-20 11:19:47 +0200

    [Feature #164680522] Api enable group owner to remove user

--- server/router/Group.js
+++ server/router/Group.js
@@ -3,9 +3,11 @@ import authv2 from '../db/middleware/auth'
 import group from '../db/controllers/Group'
 import {apiUrlv2createGroup,
   apiUrlv2deleteGroup,
-  apiUrlv2AddUserToGroup} from '../helpers/const'
+  apiUrlv2AddUserToGroup,
+  apiUrlv2DeleteUserFromGroup} from '../helpers/const'
 const router = express.Router();
 router.post(`${apiUrlv2createGroup}`,authv2.verifyToken,group.createGroup);
 router.delete(`${apiUrlv2deleteGroup}`,authv2.verifyToken,group.deletegroup);
 router.post(`${apiUrlv2AddUserToGroup}`,authv2.verifyToken,group.addUserToGroup);
+router.delete(`${apiUrlv2DeleteUserFromGroup}`,authv2.verifyToken,group.deleteUserFromGroup);
 export default router;

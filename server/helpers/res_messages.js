9cd40e7e73ec7e8ea413db1f1edd4441a87f6ea6
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-20 11:19:15 +0200

    [Feature #164680522] Added responces messages

--- server/helpers/res_messages.js
+++ server/helpers/res_messages.js
@@ -12,8 +12,10 @@ export default {
     MSG_PRGS_MESSAGE_DELETE :{"message":"Unfortunately!! You are not sender or receiver of this message, so you can not delete it."},
     MSG_DEL_SUCCESSFUL : {"message":"Message Deleted Successful "},
     MSG_GROUP_DELETED_SUCCESSFUL : {"message":"Group Deleted successfuly "},
+    MSG_GROUP_MEMBER_DELETED_SUCCESSFUL : {"message":"Member group deleted successfuly"},
     MSG_PRGS_MESSAGE_VIEW : {"message":"Unfortunately!! You are not sender or receiver of this message, so you can not open it."},  
     MSG_PRGS_DELETE_GROUP : {"message":"Unfortunately!! You are not the owner of this group, so you can not delete it."},  
-    MSG_PRGS_ADD_USER_GROUP : {"message":"Unfortunately!! You are not the owner of this group, so you can not add user to this group."}  
+    MSG_PRGS_ADD_USER_GROUP : {"message":"Unfortunately!! You are not the owner of this group, so you can not add user to this group."},  
+    MSG_PRGS_DELETE_USER_GROUP : {"message":"Unfortunately!! You are not the owner of this group, so you can not remove any user from it."}  
 
 }

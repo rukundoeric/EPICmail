0e14dbe3903df59cc4cf664e283375cf57f48ca2
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2019-03-20 10:02:45 +0200

    [Feature #164680500] add user to group router

--- server/router/Group.js
+++ server/router/Group.js
@@ -2,8 +2,10 @@ import express from 'express';
 import authv2 from '../db/middleware/auth'
 import group from '../db/controllers/Group'
 import {apiUrlv2createGroup,
-  apiUrlv2deleteGroup} from '../helpers/const'
+  apiUrlv2deleteGroup,
+  apiUrlv2AddUserToGroup} from '../helpers/const'
 const router = express.Router();
 router.post(`${apiUrlv2createGroup}`,authv2.verifyToken,group.createGroup);
 router.delete(`${apiUrlv2deleteGroup}`,authv2.verifyToken,group.deletegroup);
+router.post(`${apiUrlv2AddUserToGroup}`,authv2.verifyToken,group.addUserToGroup);
 export default router;
